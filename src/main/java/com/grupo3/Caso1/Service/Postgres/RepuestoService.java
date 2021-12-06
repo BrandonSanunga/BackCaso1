e1736a84ca150cae5a085b82cda86f5885e9601a
Marcelo Vidal <47618905+keke1917@users.noreply.github.com>
2021-12-05 23:47:26 -0500

    endpoint para filtrar repuestos por ID de disenio

--- src/main/java/com/grupo3/Caso1/Service/Postgres/RepuestoService.java
+++ src/main/java/com/grupo3/Caso1/Service/Postgres/RepuestoService.java
@@ -3,6 +3,9 @@ package com.grupo3.Caso1.Service.Postgres;
 import com.grupo3.Caso1.Commons.GenericService;
 import com.grupo3.Caso1.Model.Repuestos;
 
-public interface RepuestoService extends GenericService<Repuestos, String>{
+import java.util.ArrayList;
 
+
+public interface RepuestoService extends GenericService<Repuestos, String> {
+    ArrayList<Repuestos> findAllByIdDisenio(String idDisenio);
 }
