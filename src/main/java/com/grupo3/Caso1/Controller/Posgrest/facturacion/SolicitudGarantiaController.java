946f01aa3614cf35b826256b7ec9f7c604b98455
DCG1802 <davidcg20123@gmail.com>
2021-12-07 14:50:33 -0500

    Cambios en el controlador de SolicitudGarantia

--- src/main/java/com/grupo3/Caso1/Controller/Posgrest/facturacion/SolicitudGarantiaController.java
+++ src/main/java/com/grupo3/Caso1/Controller/Posgrest/facturacion/SolicitudGarantiaController.java
@@ -1,11 +1,15 @@
 package com.grupo3.Caso1.Controller.Posgrest.facturacion;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,16 @@ public class SolicitudGarantiaController {
 	@GetMapping
 	private ResponseEntity<List<SolicitudGarantia>> getAllSoliGarantia(){
 		return ResponseEntity.ok(garantiaService.getAll());
+	}
+	
+	@PostMapping
+	private ResponseEntity<SolicitudGarantia> saveSoliGarantia(@RequestBody SolicitudGarantia solicitud){
+ 		try {
+ 			SolicitudGarantia solicitudGar = garantiaService.save(solicitud);
+ 			return ResponseEntity.created(new URI("/solicitud/garantia/api/v1"+solicitudGar.getId_solicitud())).body(solicitudGar);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
 	}
 }
