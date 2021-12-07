58f19b864c68f4312f63a79f36e37c9a22c1431c
skull <jhosepandres14@gmail.com>
2021-12-06 22:14:44 -0500

    crud factura lsito
    cambiar la version de java en el pom.xml

--- src/main/java/com/grupo3/Caso1/Model/Vehiculo.java
+++ src/main/java/com/grupo3/Caso1/Model/Vehiculo.java
@@ -20,6 +20,8 @@ public class Vehiculo {
 	private String cilindraje;
 	private String numero_motor;
 	private String ramv;
+	private Double precio_compra;
+	private Double precio_venta;
 
 	public Long getId() {
 		return id;
@@ -104,5 +106,23 @@ public class Vehiculo {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
+	public Double getPrecio_compra() {
+		return precio_compra;
+	}
+
+	public void setPrecio_compra(Double precio_compra) {
+		this.precio_compra = precio_compra;
+	}
+
+	public Double getPrecio_venta() {
+		return precio_venta;
+	}
+
+	public void setPrecio_venta(Double precio_venta) {
+		this.precio_venta = precio_venta;
+	}
+	
+	
 
 }
